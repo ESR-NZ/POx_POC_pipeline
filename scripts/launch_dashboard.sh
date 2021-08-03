8ad8479109cfbd01aa482e1ebfd4e39b44614d71
sirselim <miles.benton84@gmail.com>
2021-08-04 09:31:27 +1200

    modified some paths in dashboard launcher script

--- scripts/launch_dashboard.sh
+++ scripts/launch_dashboard.sh
@@ -4,10 +4,11 @@
 # M Benton - ESR
 # 30/07/2021
 
-# this script should identify a systems default browser, enter a previously installed conda r_env, and 
+# this script should identify a systems default browser, enter a previously installed conda r_env, and
 # require all options and packages to launch an interactive dashboard in a local browser
 # this need to be system specific C
-source $CONDA_PATH 
+source $CONDA_PATH
+# source /xavier_ssd/miniconda3/etc/profile.d/conda.sh
 
 RESULTSDIR=$1
 
@@ -19,10 +20,9 @@ DEFAULTBROWS=$(xdg-settings get default-web-browser | sed 's/\.desktop//g')
 # actually load the thing
 conda activate r_env; R -e "options(shiny.port = 8100);                     # activate r_env, set the port to 8100 ("static")
   options(browser=\"$DEFAULTBROWS\");                                       # set default browser
-  setwd(\"dashboard\");                                                     # mv to correct dur
   require(flexdashboard);                                                   # package required to open/run
   require(rmarkdown);                                                       # package required to open/run
-  run(file=\"dashboard.Rmd\", shiny_args = list(launch.browser = TRUE))" \
+  run(file=\"./dashboard/dashboard.Rmd\", shiny_args = list(launch.browser = TRUE))" \
   --args "$RESULTSDIR"    # launch the server directly in browser
 
-# dashboard.Rmd contains a call to shiny server that will stop the server on the closing of the browser tab
+# dashboard.Rmd contains a call to shiny server that will stop the server on the closing of the browser tab or browser
